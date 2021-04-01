625615d757602d20742768183cc55a80f67ddaae
Milind Yadav <yadavmilind08@gmail.com>
2021-04-01 14:43:59 +0530

    Observable errors

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,12 +19,18 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);  // next is to emit new value
+        if (count > 3) {
+          observer.error(new Error("Count is greater than 3!"))
+        }
         count++;
       }, 1000)
     });
 
     this.firstObsSubscription = customIntervalObservable.subscribe(data => {
       console.log(data);
+    }, error => {
+      console.log(error);
+      alert(error.message);
     })
   }
 
